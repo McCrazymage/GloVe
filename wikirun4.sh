5296ce9427740681970d0c68a660d0a821eaa2ff
Li Qi <crazymage@pku.edu.cn>
2016-04-14 20:08:31 +0800

    update

--- wikirun4.sh
+++ wikirun4.sh
@@ -1,1 +1,1 @@
-nohup ./glove -vector-size 50 -threads 5 -iter 25 -save-file glove50.wiki.txt -input-file cooccurrence.shuf.wiki.bin -binary 0 -vocab-file vocab.wiki.txt -verbose 2 &
+nohup ./glove -vector-size 50 -threads 5 -iter 25 -save-file glove50.wiki -input-file cooccurrence.shuf.wiki.bin -binary 0 -vocab-file vocab.wiki.txt -verbose 2 &
