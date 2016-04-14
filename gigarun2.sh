61a6d8dae161df4564e1f41e8d21bb2283c25974
Li Qi <crazymage@pku.edu.cn>
2016-04-14 14:23:31 +0800

    update scripts

--- gigarun2.sh
+++ gigarun2.sh
@@ -1,1 +1,1 @@
-nohup ./cooccur -memory 4.0 -verbose 2 -vocab-file vocab.giga.txt -window-size 5 < ../giga.en.utf8.clean > cooccurrence.giga.bin
+./cooccur -memory 4.0 -verbose 2 -vocab-file vocab.giga.txt -window-size 5 < ../giga.en.utf8.clean > cooccurrence.giga.bin
--- wikirun2.sh
+++ wikirun2.sh
@@ -1,1 +1,1 @@
-nohup ./cooccur -memory 4.0 -verbose 2 -vocab-file vocab.wiki.txt -window-size 5 < ../enwiki-20160305-pages-articles.txt > cooccurrence.wiki.bin &
+./cooccur -memory 4.0 -verbose 2 -vocab-file vocab.wiki.txt -window-size 5 < ../enwiki-20160305-pages-articles.txt > cooccurrence.wiki.bin 
